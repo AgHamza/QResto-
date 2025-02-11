bf1d8cab0d6c5dfe0d0f6009f6c3c6a2d47a322c
AgHamza <hamzaago66@gmail.com>
2025-02-11 04:43:15 +0100

    Updated for Render hosting and database initialization

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+from . import db
+import os
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()  # This creates tables if they don't exist
 
 if __name__ == '__main__':
     app.run(debug=True)
